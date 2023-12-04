a91b9191fb89a5de5350fa86e8dfef723beec725
synthetic <pendencecode@gmail.com>
2023-12-04 20:10:17 +0100

    we're so back, day 1 done

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,19 +1,13 @@
+/*
+decoupled functionality cuz i want my API to be this ugly
+maybe u would want to read a single line, maybe line n°42069, who knows ?
+ */
 pub mod part_1 {
     pub fn parse_line(input: String) -> String {
         let mut digits = input.chars().filter(|x| x.is_ascii_digit());
         let first = digits.next().unwrap_or('0');
         let second = digits.last().unwrap_or(first);
         format!("{first}{second}")
-    }
-
-    pub fn trebuchet(content: String) -> u32 {
-        let mut sum: u32 = 0;
-        for line in content.lines() {
-            println!("we have {line}");
-            sum += parse_line(line.to_string()).as_str().parse::<u32>().unwrap();
-            println!("sum is {sum}");
-        }
-        sum
     }
 
     #[test]
@@ -24,8 +18,123 @@ pub mod part_1 {
         assert_eq!(parse_line(String::from("treb7uchet")), "77");
     }
 
+    pub fn trebuchet(content: String) -> u32 {
+        let mut sum: u32 = 0;
+        for line in content.lines() {
+            sum += parse_line(line.to_string())
+                .as_str()
+                .parse::<u32>()
+                .unwrap();
+        }
+        sum
+    }
+
     #[test]
     fn test_trebuchet() {
-        assert_eq!(trebuchet(std::fs::read_to_string("src/test_trebuchet.txt").unwrap()), 142);
+        assert_eq!(
+            trebuchet(std::fs::read_to_string("src/txts/testxt/test_trebuchet.txt").unwrap()),
+            142
+        );
+    }
+}
+
+pub mod part_2 {
+    use std::collections::HashMap;
+
+    pub fn parse_line_2(input: String) -> String {
+        let calibrations: HashMap<&str, char> = HashMap::from([
+            ("one", '1'),
+            ("two", '2'),
+            ("three", '3'),
+            ("four", '4'),
+            ("five", '5'),
+            ("six", '6'),
+            ("seven", '7'),
+            ("eight", '8'),
+            ("nine", '9'),
+        ]);
+
+        let mut first: char = '0';
+        let mut second: char = '0';
+
+        'check_first: for (index, i) in input.chars().enumerate() {
+            if i.is_ascii_digit() {
+                first = i;
+                break 'check_first;
+            } else {
+                for (key, value) in calibrations.iter() {
+                    if input[index..input.len()].starts_with(*key) {
+                        first = *value;
+                        break 'check_first;
+                    } 
+                }
+            }
+        }
+
+        'check_last: for (index, i) in input.chars().rev().enumerate() {
+            if i.is_ascii_digit() {
+                second = i;
+                break 'check_last;
+            } else {
+                for (key, value) in calibrations.iter() {
+                    if input[input.len() - index - 1..input.len()].starts_with(*key) {
+                        second = *value;
+                        break 'check_last;
+                    }
+                }
+            }
+            if second == '0' {
+                second = first;
+            }
+        }
+        format!("{first}{second}")
+    }
+
+    #[test]
+    fn test_parse_line_2() {
+        assert_eq!(parse_line_2(String::from("two1nine")), String::from("29"));
+        assert_eq!(
+            parse_line_2(String::from("eightwothree")),
+            String::from("83")
+        );
+        assert_eq!(
+            parse_line_2(String::from("abcone2threexyz")),
+            String::from("13")
+        );
+        assert_eq!(
+            parse_line_2(String::from("xtwone3four")),
+            String::from("24")
+        );
+        assert_eq!(
+            parse_line_2(String::from("4nineeightseven2")),
+            String::from("42")
+        );
+        assert_eq!(
+            parse_line_2(String::from("zoneight234")),
+            String::from("14")
+        );
+        assert_eq!(
+            parse_line_2(String::from("7pqrstsixteen")),
+            String::from("76")
+        );
+    }
+
+    pub fn trebuchet_2(content: String) -> u32 {
+        let mut sum: u32 = 0;
+        for line in content.lines() {
+            sum += parse_line_2(line.to_string())
+                .as_str()
+                .parse::<u32>()
+                .unwrap();
+        }
+        sum
+    }
+
+    #[test]
+    fn test_trebuchet_2() {
+        assert_eq!(
+            trebuchet_2(std::fs::read_to_string("src/txts/testxt/test_trebuchet_2.txt").unwrap()),
+            281
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use std::fs;
+/*
+todo:
+- an args system to execute a day-part individually 
+- reorganize the module tree somehow idk
+
+notes:
+- yes i used Strings as args and i keep convernting between Strings, &str and slices, 
+cry about it, cuz i def did.
+*/
+
 
 mod day_1;
 
 fn main() {
-    let d1_p1_sol = day_1::part_1::trebuchet(fs::read_to_string("src/day1_p1.txt").unwrap()); 
-    println!("{d1_p1_sol}");
-    
+    let d1_p1_sol =
+        day_1::part_1::trebuchet(fs::read_to_string("src/txts/inputxt/day1_p1.txt").unwrap());
+    println!("day 1 part 1: {d1_p1_sol}");
+
+    let d1_p2_sol =
+        day_1::part_2::trebuchet_2(fs::read_to_string("src/txts/inputxt/day1_p2.txt").unwrap());
+    println!("day 1 part 2: {d1_p2_sol}");
 }
